1a765269fabf0936c7fa14b229c7b1b539596448
Matthew L Davis <mld.0@protonmail.com>
2020-12-11 04:03:42 +1100

    add link to snek tutorial

--- run-setup.sh
+++ run-setup.sh
@@ -2,6 +2,9 @@
 [ -z $mld_loaded_exports ] && source "$HOME/._exports.sh"
 [ -z $mld_loaded_bins ] && source "$HOME/._bins.sh"
 
+#	See:
+#	LINK: https://amir.rachum.com/blog/2017/07/28/python-entry-points/
+
 #	When running with pyenv python, add to PATH the directory (see below) where the executibles will be placed, i.e:
 #>%		PATH=$PATH:$( dirname $( pyenv which python ) )
 
